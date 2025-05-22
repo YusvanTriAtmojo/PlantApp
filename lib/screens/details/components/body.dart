f95ffd51444e5d86fff923e98457262d5c9c8a76
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-05-23 00:28:46 +0700

    Tampilan body detail dengan ImageAndIcons dan TitleAndPrice

---
+++ lib/screens/details/components/body.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import 'image_and_icons.dart';
+import 'title_and_price.dart';
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          ImageAndIcons(size: size),
+          TitleAndPrice(title: "Angelica", country: "Russia", price: 440),
+          SizedBox(height: 20),
+          Row(
+            children: <Widget>[
+              SizedBox(
+                width: size.width / 2,
+                height: 84,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                    backgroundColor: Color(0xFF0C9869),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Buy Now",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Description",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
