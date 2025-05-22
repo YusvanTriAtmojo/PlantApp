c415b8484860e0049c8591353f85a2351af496fb
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-05-22 23:51:27 +0700

    Membuat tampilan body dengan header, title, recomend, plantcard

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/screens/home/components/header_with_searchbox.dart';
+import 'featured_plant.dart';
+import 'recomend_plant.dart';
+import 'title_with_more_btn.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          HeaderWithSearchbox(size: size),
+          TitleWithMoreBtn(title: "Recomended", press: () {}),
+          RecomendPlant(),
+          TitleWithMoreBtn(title: "Featured Plants", press: () {}),
+          FeaturedPlant(),
+          SizedBox(height: 20),
+        ],
+      ),
+    );
   }
 }
