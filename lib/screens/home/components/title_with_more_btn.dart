3ead16ec2e83c1110a125c75b5043be51e575d76
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-05-22 23:08:43 +0700

    Menambahkan underline berwarna hijau untuk text atau title

--- lib/screens/home/components/title_with_more_btn.dart
+++ lib/screens/home/components/title_with_more_btn.dart
@@ -16,6 +16,7 @@ class TitleWithMoreBtn extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Row(
         children: <Widget>[
+          TitleWithCustomUnderline(text: title),
           const Spacer(),
           TextButton(
             style: TextButton.styleFrom(
@@ -30,6 +31,43 @@ class TitleWithMoreBtn extends StatelessWidget {
               style: TextStyle(color: Colors.white),
             ),
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithCustomUnderline extends StatelessWidget {
+  const TitleWithCustomUnderline({
+    super.key,
+    required this.text,
+  });
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 30,
+      child: Stack(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 5),
+            child: Text(
+              text,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: const EdgeInsets.only(right: 5),
+              height: 7,
+              color: const Color(0xFF0C9869).withAlpha(51) 
+            ),
+          )
         ],
       ),
     );
