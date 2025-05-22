54b8a6ceb409fe2609f2d5ac78f95b52e797f7f4
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-05-23 00:29:47 +0700

    Memanggil Body Detail

--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/screens/details/components/body.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key});
@@ -6,6 +7,7 @@ class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Body(),
     );
   }
 }
